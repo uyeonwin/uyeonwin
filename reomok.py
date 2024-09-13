9ced9a3c46694725e113834241229267c5f8bf59
uyeonwin <su061001@gmail.com>
2024-09-13 10:28:16 +0900

    Feat: 배경창 띄우기 Rename: 배경 축소

--- reomok.py
+++ reomok.py
@@ -2,13 +2,17 @@ import pygame
 
 pygame.init()
 
-background = pygame.display.set_mode((200,100))
+background = pygame.display.set_mode((700,762))
 pygame.display.set_caption("dfd")
+
+image_bg = pygame.image.load("image/scorebg.png")
 
 play = True
 while play:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             play = False
+    background.blit(image_bg, (0,0))
+    pygame.display.update()
 
 pygame.quit()
